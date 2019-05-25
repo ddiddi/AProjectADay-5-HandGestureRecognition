ff9bb8220a3f6302d006e32e295e1c20541ffbfc
Dhruv Diddi <dhruv.diddi+1@gmail.com>
2019-05-24 20:40:24 -0700

    Set up mask for background segmentation

--- gesture_detection.py
+++ gesture_detection.py
@@ -15,7 +15,31 @@ cv2.namedWindow('track')
 cv2.createTrackbar('tr1','track',threshold,100,print_threshold)
 
 while cam.isOpened():
+
+    frame_ret, frame = cam.read()
+    threshold = cv2.getTrackbarPos('tr1','track')
+
+    smooth_frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
+    flip_frame = cv2.flip(smooth_frame, 1)  # flip the frame horizontally
+    cv2.imshow('Frame', flip_frame)
+
+    if background_flag == True:
+        # Create background mask from input
+        mask = background.apply(flip_frame,learningRate=0)
+        kernel = np.ones((3, 3), np.uint8)
+        mask = cv2.erode(mask, kernel, iterations=1)
+        mask_image = cv2.bitwise_and(flip_frame, flip_frame, mask=mask)
+        cv2.imshow('mask', mask_image)
+
+        # Process image into thresholding for hand detection
+        gray_mask = cv2.cvtColor(mask_image, cv2.COLOR_BGR2GRAY)
+        blur_mask = cv2.GaussianBlur(gray_mask, (40, 40), 0)
+        cv2.imshow('blur', blur_mask)
+        ret, thresh = cv2.threshold(blur_mask, threshold, 255, cv2.THRESH_BINARY)
+        cv2.imshow('threshold', thresh)
+
     key_press = cv2.waitKey(10)
+
     # Check if 'escape' key is pressed
     if key_press == 27:
         cam.release()
