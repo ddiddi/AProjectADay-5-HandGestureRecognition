61fd73cf56b13b8343bb5f9a09018d4bdd7369dd
Dhruv Diddi <dhruv.diddi+1@gmail.com>
2019-05-24 20:24:29 -0700

    Set key interface

--- gesture_detection.py
+++ gesture_detection.py
@@ -3,10 +3,31 @@ import numpy as np
 import copy
 import math
 
+threshold = 20
+def print_threshold(threshold):
+    print("Binary threshold: "+str(thresh))
 
 # Setup video camera OpenCV object to get input stream from webcam
 cam = cv2.VideoCapture(0)
 # Set some size
-cam.set(10,100)
+cam.set(10,250)
 cv2.namedWindow('track')
-cv2.createTrackbar('tr1','track',threshold,100)
+cv2.createTrackbar('tr1','track',threshold,100,print_threshold)
+
+while cam.isOpened():
+    key_press = cv2.waitKey(10)
+    # Check if 'escape' key is pressed
+    if key_press == 27:
+        cam.release()
+        cv2.destroyAllWindows()
+        break
+    # Check if 'b' key is pressed to capture background
+    elif k == ord('b'):
+        background = cv2.createBackgroundSubtractorMOG2(0, background_threshold)
+        background_flag = True
+        print( 'Background Set \n')
+    # Check if 'r' key is pressed to refresh background
+    elif k == ord('r'):
+        background = None
+        background_flag = False
+        print ('Background Reset \n')
